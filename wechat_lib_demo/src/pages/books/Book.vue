587b152e3cbba6ec9c16bb9dcfcad0a7758bd369
yangyafei <342363079@qq.com>
2018-08-26 02:23:58 +0800

    5-8配置bar，新增icon

---
+++ wechat_lib_demo/src/pages/books/Book.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="">
+    图书列表页面
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  components: {
+
+  },
+
+  methods: {
+
+  },
+
+  created () {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ wechat_lib_demo/src/pages/comments/Comment.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="">
+    评论过的书页面
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  components: {
+
+  },
+
+  methods: {
+
+  },
+
+  created () {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
