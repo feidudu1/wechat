12c44a831d3640b1c705f2373463aa4943f1bd9c
yangyafei <342363079@qq.com>
2018-09-08 17:01:50 +0800

    个人中心

--- wechat_lib_demo/src/App.vue
+++ wechat_lib_demo/src/App.vue
@@ -1,11 +1,45 @@
 <script>
+import config from '@/config'
+import {get, showSuccess} from './util'
+import qcloud from 'wafer2-client-sdk'
+
 export default {
-  created () {
-    console.log('小程序启动了');
-  }
+  // async created () {
+  //   let user = wx.getStorageSync('userinfo')
+  //   if (!user) {
+  //     // 设置登录地址
+  //     qcloud.setLoginUrl(config.loginUrl);
+  //     qcloud.login({
+  //         success: function (userinfo) {
+  //             console.log('登录成功', userinfo);
+  //             showSuccess('登录成功')
+  //             wx.setStorageSync('userinfo', userinfo)
+  //         },
+  //         fail: function (err) {
+  //             console.log('登录失败', err);
+  //         }
+  //     });
+  //   }
+  //   // const res = await get('/weapp/demo')
+  //   // console.log(123, res);
+  //   // console.log('小程序启动了');
+  // }
 }
 </script>
 
 <style>
-
+  .btn {
+    color: white;
+    background: #EA5A49;
+    margin-bottom: 10px;
+    padding: 0 15px;
+    border-radius: 2px;
+    font-size: 16px;
+    line-height: 40px;
+    height: 40px;
+    width: 100%;
+  }
+  .btn:active {
+    background: #FA5A49;
+  }
 </style>
--- wechat_lib_demo/src/pages/me/Me.vue
+++ wechat_lib_demo/src/pages/me/Me.vue
@@ -1,32 +1,87 @@
 <template>
-  <div class="">
-    个人中心页面
+  <div class="container">
+    <div class="userinfo" @click="login">
+      <img :src="userinfo.avatarUrl" alt="">
+      <p>{{userinfo.nickName}}</p>
+    </div>
+    <YearProgress/>
+    <button v-if="userinfo.openId" class="btn" @click="scanBook">添加图书</button>
   </div>
+
 </template>
 
 <script>
+import YearProgress from '@/components/YearProgress'
+import config from '@/config'
+import {get, showSuccess} from '@/util'
+import qcloud from 'wafer2-client-sdk'
 
 export default {
   data () {
     return {
-
+      userinfo: {
+        avatarUrl: '@/static/img/unlogin.png'
+      }
     }
   },
 
   components: {
-
+    YearProgress
   },
 
   methods: {
-
+    scanBook () {
+      wx.scanCode({
+        success: (res) => {
+          console.log(res);
+        }
+      })
+    },
+    async login () {
+      let user = wx.getStorageSync('userinfo')
+      let that = this
+      if (!user) {
+        // 设置登录地址
+        qcloud.setLoginUrl(config.loginUrl);
+        qcloud.login({
+            success: function (userinfo) {
+                console.log('登录成功', userinfo);
+                showSuccess('登录成功')
+                wx.setStorageSync('userinfo', userinfo)
+                that.userinfo = userinfo
+            },
+            fail: function (err) {
+                console.log('登录失败', err);
+            }
+        })
+      }
+    },
+    onShow() {
+      let userinfo = wx.getStorageSync('userinfo')
+      if (userinfo) {
+        this.userinfo = userinfo
+      }
+    }
   },
 
   created () {
-
+    this.userinfo = wx.getStorageSync('userinfo')
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .container {
+    padding: 0 30rpx;
+    .userinfo {
+      margin-top: 100rpx;
+      text-align: center;
+      img {
+        width: 150rpx;
+        height: 150rpx;
+        margin: 20rpx;
+        border-radius: 50%;
+      }
+    }
+  }
 </style>
