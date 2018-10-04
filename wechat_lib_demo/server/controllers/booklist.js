67635001b5e5e8e680b178de16f93f3d4fbeeeff
yangyafei <342363079@qq.com>
2018-10-04 13:52:38 +0800

    8-4 登陆和数据库联合查询
    官方登陆逻辑修改 Me.vue

--- wechat_lib_demo/server/controllers/booklist.js
+++ wechat_lib_demo/server/controllers/booklist.js
@@ -2,8 +2,19 @@ const https = require('https')
 const {mysql} = require('../qcloud')
 
 module.exports = async (ctx) => {
-  const book = await mysql('books').select('*').orderBy('id', 'desc')
+  const book = await mysql('books')
+    .select('books.*', 'cSessionInfo.user_info')
+    .join('cSessionInfo', 'books.openid', 'cSessionInfo.open_id')
+    .orderBy('books.id', 'desc')
+  // .orderBy('id', 'desc')
   ctx.state.data = {
-    list: book
+    list: book.map(v => {
+      const info = JSON.parse(v.user_info)
+      return Object.assign({}, v, {
+        user_info: {
+          nickName: info.nickName
+        }
+      })
+    })
   }
 }
--- wechat_lib_demo/src/pages/me/Me.vue
+++ wechat_lib_demo/src/pages/me/Me.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="container">
-    <div class="userinfo" @click="login">
+    <div class="userinfo">
       <img :src="userinfo.avatarUrl" alt="">
       <p>{{userinfo.nickName}}</p>
     </div>
     <YearProgress/>
-    <button v-if="userinfo.openId" class="btn" @click="scanBook">添加图书</button>
+    <button v-if="userinfo.openId" class="btn btn-addbook" @click="scanBook">添加图书</button>
+    <button v-else open-type="getUserInfo" lang="zh_CN" class='btn' @getuserinfo="login">点击登录</button>
   </div>
 
 </template>
@@ -13,28 +14,43 @@
 <script>
 import YearProgress from '@/components/YearProgress'
 import config from '@/config'
-import {get, showSuccess, post, showModal} from '@/util'
+import {showSuccess, post, get, showModal} from '@/util'
 import qcloud from 'wafer2-client-sdk'
 
 export default {
   data () {
     return {
-      userinfo: {
-        avatarUrl: '@/static/img/unlogin.png'
-      }
+      // userinfo: {
+      //   avatarUrl: '@/static/img/me.png',
+      //   nickName: ''
+      // }
     }
   },
 
   components: {
     YearProgress
   },
-
+  computed: {
+    userinfo () {
+      let user = wx.getStorageSync('userinfo')
+      if (user) {
+        return user
+      } else {
+        return {
+          avatarUrl: '@/static/img/me.png',
+          nickName: ''
+        }
+      }
+    }
+  },
   methods: {
     async addBook (isbn) {
       const res = await post('/weapp/addbook', {
         isbn,
         openid: this.userinfo.openId
       })
+      console.log('res', res);
+      debugger
       if (res.code === 0 && res.data.title) {
         showModal('添加成功', `${res.data.title}添加成功`)
       //   showSuccess('添加成功', res.data.title + '添加成功')
@@ -44,7 +60,7 @@ export default {
       wx.scanCode({
         success: (res) => {
           if (res.result) {
-            console.log('hi', res.result);
+            console.log('扫描成功', res.result);
             this.addBook(res.result)
           }
         }
@@ -57,15 +73,15 @@ export default {
         // 设置登录地址
         qcloud.setLoginUrl(config.loginUrl);
         qcloud.login({
-            success: function (userinfo) {
-                console.log('登录成功', userinfo);
-                showSuccess('登录成功')
-                wx.setStorageSync('userinfo', userinfo)
-                that.userinfo = userinfo
-            },
-            fail: function (err) {
-                console.log('登录失败', err);
-            }
+          success: function (userinfo) {
+              console.log('登录成功信息', userinfo);
+              showSuccess('登录成功')
+              wx.setStorageSync('userinfo', userinfo)
+              that.userinfo = userinfo
+          },
+          fail: function (err) {
+              console.log('登录失败', err);
+          }
         })
       }
     },
@@ -75,10 +91,6 @@ export default {
         this.userinfo = userinfo
       }
     }
-  },
-
-  created () {
-    this.userinfo = wx.getStorageSync('userinfo')
   }
 }
 </script>
